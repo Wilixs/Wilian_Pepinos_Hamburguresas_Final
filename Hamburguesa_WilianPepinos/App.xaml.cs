20bc82d4198e76f752bbf801b25c401c14232aa1
Wilixs <115099196+Wilixs@users.noreply.github.com>
2023-01-16 00:38:07 -0500

    Agregar archivos de proyecto.

---
+++ Hamburguesa_WilianPepinos/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Hamburguesa_WilianPepinos.Data;
+
+namespace Hamburguesa_WilianPepinos;
+
+public partial class App : Application
+{
+
+    public static BurgerDatabase BurgerRepo { get; private set; }
+
+    public App(BurgerDatabase repo)
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+
+        BurgerRepo = repo;
+    }
+}
---
+++ Hamburguesa_WilianPepinos/Views/BurgerListPage.xaml.cs
@@ -0,0 +1,32 @@
+using Hamburguesa_WilianPepinos.Models;
+
+namespace Hamburguesa_WilianPepinos.Views;
+
+public partial class BurgerListPage : ContentPage
+{
+    public BurgerListPage()
+    {
+        InitializeComponent();
+        List<Burger> burger = App.BurgerRepo.GetAllBurgers();
+        burgerList.ItemsSource = burger;
+    }
+    async void OnItemAdded(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(BurgerItemPage));
+    }
+
+    private void RefreshData(object sender, EventArgs e)
+    {
+        List<Burger> burger = App.BurgerRepo.GetAllBurgers();
+        burgerList.ItemsSource = burger;
+    }
+    private async void SelectedItem(object sender, SelectionChangedEventArgs e)
+    {
+        if (e.CurrentSelection.Count != 0)
+        {
+            var burgers = (Models.Burger)e.CurrentSelection[0];
+            await Shell.Current.GoToAsync($"{nameof(BurgerItemPage)}?{nameof(BurgerItemPage.ItemId)}={burgers.Id}");
+            burgerList.SelectedItem = null;
+        }
+    }
+}
